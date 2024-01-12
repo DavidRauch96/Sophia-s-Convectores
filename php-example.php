51c35490b47d5933086ac904dda38ffce863f0fe
DavidRauch96 <david.rauch@t-online.de>
2024-01-12 22:45:22 +0100

    php tryouts

--- php-example.php
+++ php-example.php
@@ -5,5 +5,31 @@
   $name  = htmlspecialchars($_POST['name']);
   $entry  = htmlspecialchars($_POST['entry']);
 
-  echo  $date, ' ', $name, ' ', $entry;
+  echo  $date <br>;
+  echo  $name <br>;
+  echo  $entry;
+
+/*<?php
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Retrieve form data
+  $date = $_POST["date"];
+  $name = $_POST["name"];
+  $entry = $_POST["entry"];
+
+  // Validate data (you may want to add more validation)
+  if (empty($date) || empty($name) || empty($entry)) {
+      echo "Please fill out all fields.";
+  } else {
+      // Display the new entry
+      echo '<li class="entryitem center">';
+      echo '<div>';
+      echo "<p># $name am $date</p>";
+      echo "<p>$entry</p>";
+      echo '</div>';
+      echo '</li>';
+  }
+}
+?>*/
+
 ?>
