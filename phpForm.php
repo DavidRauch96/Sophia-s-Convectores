a9b807c6e8cd3230e6ef5899e85c2795924098c3
DavidRauch96 <david.rauch@t-online.de>
2024-01-16 23:36:26 +0100

    php test

--- phpForm.php
+++ phpForm.php
@@ -1,35 +1,24 @@
 <?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-  $date = htmlspecialchars($_POST['date']);
-  $name  = htmlspecialchars($_POST['name']);
-  $entry  = htmlspecialchars($_POST['entry']);
 
-  echo  $date <br>;
-  echo  $name <br>;
-  echo  $entry;
+$name = $date = $entry = "";
 
-/*<?php
-// Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Retrieve form data
-  $date = $_POST["date"];
-  $name = $_POST["name"];
-  $entry = $_POST["entry"];
-
-  // Validate data (you may want to add more validation)
-  if (empty($date) || empty($name) || empty($entry)) {
-      echo "Please fill out all fields.";
-  } else {
-      // Display the new entry
-      echo '<li class="entryitem center">';
-      echo '<div>';
-      echo "<p># $name am $date</p>";
-      echo "<p>$entry</p>";
-      echo '</div>';
-      echo '</li>';
-  }
+  $name = test_input($_POST["name"]);
+  $date = test_input($_POST["date"]);
+  $entry = test_input($_POST["entry"]);
 }
-?>*/
 
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+echo "<h2>Your Input</h2>";
+echo $name;
+echo "<br>";
+echo $date;
+echo "<br>";
+echo $entry;
 ?>
