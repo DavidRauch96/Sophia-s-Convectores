d211defe9bdb18c65c2318b3f88b670d76d21085
DavidRauch96 <david.rauch@t-online.de>
2024-01-17 21:21:19 +0100

    delete entries.txt

--- phpForm.php
+++ phpForm.php
@@ -83,12 +83,6 @@ echo $moodErr;
 echo "<br>";
 echo $entry;
 
-/* $sql = "INSERT INTO Entries VALUES ('$name', '$date', '$mood', '$entry')";
-if (mysqli_query($conn, $sql)) {
-     echo "New record created successfully";
-} else {
-     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-} */
 
 mysqli_close($conn);
 ?>
