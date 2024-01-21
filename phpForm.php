0491496051acf41a21f2b8aa4267794f295b0168
DavidRauch96 <david.rauch@t-online.de>
2024-01-21 23:55:16 +0100

    html cleanup and indentation

--- phpForm.php
+++ phpForm.php
@@ -7,7 +7,7 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
      die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+/* echo "Connected successfully"; */
 
 /* 
  *  What happens when you click "Submit"
